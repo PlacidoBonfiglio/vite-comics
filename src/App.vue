7786dd28286d23762a7c6e352a42ebe99fa67f87
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-09-16 19:30:59 +0200

    bonus need fix

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 import AppFooter from './components/AppFooter.vue';
+import AppBonus from './components/AppBonus.vue';
 
 export default {
   data() {
@@ -12,6 +13,7 @@ export default {
   components: {
     AppHeader,
     AppMain,
+    AppBonus,
     AppFooter
   }
 }
@@ -21,6 +23,7 @@ export default {
 <template>
   <AppHeader/>
   <AppMain/>
+  <AppBonus/>
   <AppFooter/>
 </template>
 
---
+++ src/components/AppBonus.vue
@@ -0,0 +1,59 @@
+<script>
+export default {
+    data() {
+        return {
+            bonusLinks: [
+                {
+                    img: '<img src="../assets/img/buy-comics-digital-comics.png" alt="digital comics">',
+                    name: 'DIGITAL COMICS'
+                },
+                {
+                    img: '<img src="../assets/img/buy-comics-merchandise.png" alt="merchandise">',
+                    name: 'DC MERCHANDISE'
+                },
+                {
+                    img: '<img src="../assets/img/buy-comics-subscriptions.png" alt="subscription">',
+                    name: 'SUBSCRIPTION'
+                },
+                {
+                    img: '<img src="../assets/img/buy-comics-shop-locator.png" alt="shop locator">',
+                    name: 'COMIC SHOP LOCATOR'
+                },
+                {
+                    img: '<img src="../assets/img/buy-dc-power-visa.svg" alt="power visa">',
+                    name: 'DC POWER VISA'
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<template>
+   <section id="bonus">
+    <div class="container">
+        <ul>
+            <li v-for="bonus in bonusLinks">
+                <a href="#">{{ bonus.img }} {{ bonus.name }}</a>
+            </li>
+        </ul>
+    </div>
+    
+    
+   </section>
+</template>
+
+<style scoped>
+    #bonus {
+        height: 200px;
+        background-color: #0C7CEC;
+
+        img {
+            height: 60px;
+        }
+
+        a {
+            color: white;
+        }
+    }
+</style>
